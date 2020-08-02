f0d1cb9784ccb4173936c059df260e591109acd3
Joshua <joshuadelorimier@gmail.com>
2020-08-02 12:08:47 -0400

    added plane

--- public/js/app.js
+++ public/js/app.js
@@ -1,49 +1,49 @@
 'use strict'
-
-
- 
-
-const vrscene = (scene, renderer) => {
-    const mat1 = new THREE.MeshPhongMaterial({
-        wireframe: false,
-        //color: '#DA3917'
-    })
-
-    const plane =  new THREE.Mesh(new THREE.PlaneBufferGeometry(5, 20, 32), mat1);
-    plane.position.set(0,0,-10);
-    scene.add(plane);
-    
-    const light1 = new THREE.DirectionalLight('rgb(255,255,255)',0.9,5);
-   light1.position.set(-4,0,10);
-   scene.add(light1);
-}
-
 const mainVR = () => {
     const scene = new THREE.Scene();
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    let camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innnerHeight, 0.1, 1000);
+    let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innnerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ antialias: false });
     
     renderer.setSize(window.innerWidth, window.innerHeight);
-    let vr = true;
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    document.body.appendChild(renderer.domElement); 
+    let vr = false;
+    
     if(vr === true) {
         document.body.appendChild( VRButton.createButton( renderer ) );
         renderer.xr.enabled = true;
     }
+    
     const onWindowResize = () => {
         renderer.setSize( window.innerWidth, window.innerHeight );
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
     }
+    
     window.addEventListener( 'resize', onWindowResize, false );
     
+    const mat1 = new THREE.MeshLambertMaterial({
+        color: 'rgb(255,255,255)',
+        side: THREE.DoubleSide
+    });
     
-    vrscene(scene, renderer);
+    const plane =  new THREE.Mesh(new THREE.PlaneBufferGeometry(50, 20, 32), mat1);
+    plane.position.set(0, 0, -100);
+    scene.add(plane);
+    
+    const light1 = new THREE.DirectionalLight('rgb(255,255,255)',0.9,5);
+    light1.position.set(-4,0,10);
+    scene.add(light1);
     const render = () => {
         renderer.render(scene, camera);
       }
-    console.log('hello');
+    console.log('hellooo');
     const drawtoScreen = () => {
-        render();
+        renderer.setAnimationLoop(()=>{
+            render();
+        })
+       
     }
     drawtoScreen();
 }
